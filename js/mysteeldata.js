e35da10e039eaddbd4f43692ba83d73c7e571670
陈超 <chenchao@eastmoney.com>
2016-03-29 16:18:03 +0800

    整理代码

--- js/mysteeldata.js
+++ js/mysteeldata.js
@@ -9,4 +9,13 @@ $(function(){
   //todo
   console.log('page loaded!');
 
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage', '6thPage'],
+    sectionsColor: ['#0D2241', '#273A6D', '#093675', '#093675', '#093675', '#093675'],
+    navigation: true,
+    verticalCentered: false, //内容是否垂直居中
+    navigationPosition: 'right',
+    navigationTooltips: ['mysteelData1', 'mysteelData2', 'mysteelData3', 'mysteelData4', 'mysteelData5', 'mysteelData6']
+  });
+
 });
